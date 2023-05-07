55b39ce7b86ba5f078ce00c834c30e9a6d0f0cd0
Nimakel <maxim.yashyn@gmail.com>
2023-05-07 16:06:22 +0300

    added controllers

---
+++ src/main/java/com/cinema/controller/AuthenticationController.java
@@ -0,0 +1,29 @@
+package com.cinema.controller;
+
+import javax.validation.Valid;
+import com.cinema.dto.request.UserRequestDto;
+import com.cinema.dto.response.UserResponseDto;
+import com.cinema.model.User;
+import com.cinema.service.AuthenticationService;
+import com.cinema.service.mapper.ResponseDtoMapper;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthenticationController {
+    private final AuthenticationService authService;
+    private final ResponseDtoMapper<UserResponseDto, User> userDtoResponseMapper;
+
+    public AuthenticationController(AuthenticationService authService,
+            ResponseDtoMapper<UserResponseDto, User> userDtoResponseMapper) {
+        this.authService = authService;
+        this.userDtoResponseMapper = userDtoResponseMapper;
+    }
+
+    @PostMapping("/register")
+    public UserResponseDto register(@RequestBody @Valid UserRequestDto requestDto) {
+        User user = authService.register(requestDto.getEmail(), requestDto.getPassword());
+        return userDtoResponseMapper.mapToDto(user);
+    }
+}
